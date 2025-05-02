031b94f4a9c28530335e1417b0c3f3470b244125
yvespansaintlaurent <ypan399@students.fairmontschools.com>
2025-05-01 22:25:10 -0700

    inheritance

---
+++ Pan/Unit_9_Yves.java
@@ -0,0 +1,22 @@
+public class Person{
+    private String name;
+    private int age;
+
+    public person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    //getters and setters
+    public void getName(){
+        return name;
+    }
+    public void setName(String value){
+        name = value;
+    }
+    public void getAge(){
+        return age;
+    }
+    public void setAge(int setAge){
+        age = setAge;
+    }
+}

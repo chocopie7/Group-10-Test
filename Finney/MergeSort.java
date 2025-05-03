1e3e275f3b55577bf906a4dcfea5553eccd198f5
Afrancisfinney <afrancisfinney647@students.fairmontschools.com>
2025-05-02 22:49:06 -0700

    Commit

--- Finney/MergeSort.java
+++ Finney/MergeSort.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class PhaseSeparatedMergeSort {
+public class MergeSort {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(
             3, 7, 14, 7, 13, 2, 16, 13, 15, 2

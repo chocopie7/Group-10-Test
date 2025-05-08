6c65969bd9cbbfe9e2921d6cc22e21c0a61904dc
yvespansaintlaurent <ypan399@students.fairmontschools.com>
2025-05-08 13:03:20 -0700

    Update Book.java

--- Pan/Book.java
+++ Pan/Book.java
@@ -1,33 +1,21 @@
-/**
- * Book.java
- * 
- * A simple Book class for a library system.
- * Demonstrates class anatomy, constructors, getters/setters,
- * custom methods, static members, and comments.
- * 
- * @author John Doe
- * @version 1.0
- */
+
 public class Book {
-    /** Title of the book */
+    
     private String title;
     
-    /** Author of the book */
+   
     private String author;
     
-    /** Number of pages in the book */
+    
     private int pages;
     
-    /** Whether the book is currently checked out */
+    
     private boolean isCheckedOut;
     
-    /** Static counter for total number of Book objects created */
+
     private static int totalBooksCreated = 0;
     
-    /**
-     * Default constructor: sets default values.
-     * (Empty title/author, 0 pages, not checked out)
-     */
+    
     public Book() {
         title = "";
         author = "";
@@ -36,13 +24,7 @@ public class Book {
         totalBooksCreated++;
     }
     
-    /**
-     * Constructor: sets title, author, and pages based on parameters.
-     * Initially, the book is not checked out.
-     * @param title   the book's title
-     * @param author  the book's author
-     * @param pages   number of pages in the book
-     */
+    
     public Book(String title, String author, int pages) {
         this.title = title;
         this.author = author;
@@ -51,42 +33,42 @@ public class Book {
         totalBooksCreated++;
     }
     
-    /** @return the title of the book */
+    //getters and setters
     public String getTitle() {
         return title;
     }
     
-    /** @param title the new title of the book */
+    
     public void setTitle(String title) {
         this.title = title;
     }
     
-    /** @return the author of the book */
+   
     public String getAuthor() {
         return author;
     }
     
-    /** @param author the new author of the book */
+    
     public void setAuthor(String author) {
         this.author = author;
     }
     
-    /** @return the number of pages in the book */
+    
     public int getPages() {
         return pages;
     }
     
-    /** @param pages the new page count for the book */
+   
     public void setPages(int pages) {
         this.pages = pages;
     }
     
-    /** @return true if the book is checked out, false otherwise */
+    
     public boolean isCheckedOut() {
         return isCheckedOut;
     }
     
-    /** Check out the book (sets isCheckedOut to true) */
+    
     public void checkOut() {
         if (!isCheckedOut) {
             isCheckedOut = true;
@@ -96,7 +78,7 @@ public class Book {
         }
     }
     
-    /** Return the book (sets isCheckedOut to false) */
+    
     public void returnBook() {
         if (isCheckedOut) {
             isCheckedOut = false;
@@ -106,39 +88,36 @@ public class Book {
         }
     }
     
-    /** @return the total number of Book objects created */
+    
     public static int getTotalBooksCreated() {
         return totalBooksCreated;
     }
     
-    /**
-     * Main method to demonstrate the Book class.
-     * Creates Book objects and calls methods on them.
-     */
+   
     public static void main(String[] args) {
-        // Create two Book objects
+        
         Book b1 = new Book("1984", "George Orwell", 328);
         Book b2 = new Book("To Kill a Mockingbird", "Harper Lee", 281);
         
-        // Show total books created (static variable)
+        
         System.out.println("Total books created: " + Book.getTotalBooksCreated());
         
-        // Show details of first book using getters
+       
         System.out.println(b1.getTitle() + " by " + b1.getAuthor() 
                            + ", " + b1.getPages() + " pages. Checked out? " + b1.isCheckedOut());
         
-        // Check out the first book
+        
         b1.checkOut();
         System.out.println("Checked out? " + b1.isCheckedOut());
         
-        // Try checking it out again
+        
         b1.checkOut();
         
-        // Return the first book
+        
         b1.returnBook();
         System.out.println("Checked out? " + b1.isCheckedOut());
         
-        // Check out and return second book
+        
         b2.checkOut();
         b2.returnBook();
     }
